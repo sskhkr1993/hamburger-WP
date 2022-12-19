3d7498f93aa991cb7e5d1b98c16fcfb4ff9c52c1
sskhkr1993 <krkkou65@gmail.com>
2022-12-19 22:19:26 +0900

    page,single 画像表示できていないやつを修正tenplate →stylesheet

--- single.php
+++ single.php
@@ -1,187 +1,187 @@
 <?php get_header(); ?>
 
-<?php get_sidebar(); ?> 
+<?php get_sidebar(); ?>
 
-  <!--メインビジュアル-->
-  <div class="hero hero--single ">
-    <div class="hero--single__background Flexbox--column">
-      <h1 class="hero--single__heading Heading--m1bold"><?php the_title(); ?></h1>
+<!--メインビジュアル-->
+<div class="hero hero--single ">
+  <div class="hero--single__background Flexbox--column">
+    <h1 class="hero--single__heading Heading--m1bold"><?php the_title(); ?></h1>
+  </div>
+</div>
+
+<!--ここからメインコンテンツ-->
+<main class="l-main u-padding">
+
+  <!--ここからセクション-->
+  <section class="information">
+    <h2 class="information__heading--huge Heading--m1bold">見出しh2</h2>
+    <p class="information__text Content">
+      pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
+    </p>
+    <h3 class="information__heading--large Heading--m1bold">見出しh3</h3>
+    <h4 class="information__heading--medium Heading--m1bold">見出しh4</h4>
+    <h5 class="information__heading--small Heading--m1bold">見出しh5</h5>
+    <h6 class="information__heading--tiny Heading--m1bold">見出しh6</h6>
+    <figure class="quote Quote">
+      <blockquote class="quote__body">
+        <p class="quote__content Content">
+          Blockquote
+          引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
+        </p>
+      </blockquote>
+      <figcaption class="quote__footer">
+        出典元:<cite class="quote__cite Cite"><a href="#">○○○○○○○○○○○○</a></cite>
+      </figcaption>
+    </figure>
+
+    <div class="information__image-outer">
+      <img class="information__image Image" src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="ハンバーガーとポテトの写真">
+    </div>
+  </section>
+
+  <!--ここからカード-->
+  <div class="article--single">
+    <article class="card--single Grid">
+      <div class="card--single__header">
+        <div class="card--single__figure">
+          <div class="card--single__image Image">
+          </div>
+        </div>
+      </div>
+      <div class="card--single__body">
+        <p class="card--single__content Content">
+          テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+      </div>
+    </article>
+
+    <article class="card--singleSecond Grid">
+      <div class="card--singleSecond__header">
+        <div class="card--singleSecond__figure">
+          <div class="card--singleSecond__image Image">
+          </div>
+        </div>
+      </div>
+      <div class="card--singleSecond__body">
+        <p class="card--singleSecond__content Content">
+          テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+      </div>
+    </article>
+  </div>
+
+  <!--ここはimg-->
+  <div class="image__outer--single">
+    <img class="image__inner--single Image" src="<?php echo get_theme_file_uri('/image\single\cooked-foods-750073.jpg'); ?>" alt="ハンバーガーとポテトの写真">
+  </div>
+
+  <!--ここからimgリスト-->
+  <div class="image-list__wrapper">
+    <ul class="image-list Grid">
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+      <li class="image-list__item">
+        <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
+      </li>
+    </ul>
+  </div>
+
+  <!--list-->
+  <div class="list-wrapper">
+    <div class="decimal-list__outer">
+      <ol class="decimal-list">
+        <li class="decimal-list__item">リストリストリスト</li>
+        <li class="decimal-list__item">リストリストリスト
+          <ol class="decimal-list decimal-list--second">
+            <li class="decimal-list__item decimal-list__item--second">リスト2リスト2リスト2</li>
+            <li class="decimal-list__item decimal-list__item--second">リスト2リスト2リスト2</li>
+          </ol>
+        </li>
+      </ol>
+      <ol class="decimal-list">
+        <li class="decimal-list__item">リストリストリスト</li>
+        <li class="decimal-list__item">リストリストリスト</li>
+      </ol>
+    </div>
+    <div class="text-list__outer">
+      <ul class="text-list">
+        <li class="text-list__item">リストリストリスト</li>
+        <li class="text-list__item">リストリストリスト
+          <ul class="text-list text-list--second">
+            <li class="text-list__item text-list__item--second">リスト2リスト2リスト2</li>
+            <li class="text-list__item text-list__item--second">リスト2リスト2リスト2</li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="text-list">
+        <li class="text-list__item">リストリストリスト</li>
+        <li class="text-list__item">リストリストリスト</li>
+      </ul>
     </div>
   </div>
 
-  <!--ここからメインコンテンツ-->
-  <main class="l-main u-padding">
-
-    <!--ここからセクション-->
-    <section class="information">
-      <h2 class="information__heading--huge Heading--m1bold">見出しh2</h2>
-      <p class="information__text Content">
-        pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。
-      </p>
-      <h3 class="information__heading--large Heading--m1bold">見出しh3</h3>
-      <h4 class="information__heading--medium Heading--m1bold">見出しh4</h4>
-      <h5 class="information__heading--small Heading--m1bold">見出しh5</h5>
-      <h6 class="information__heading--tiny Heading--m1bold">見出しh6</h6>
-      <figure class="quote Quote">
-        <blockquote class="quote__body">
-          <p class="quote__content Content">
-            Blockquote
-            引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ
-          </p>
-        </blockquote>
-        <figcaption class="quote__footer">
-          出典元:<cite class="quote__cite Cite"><a href="#">○○○○○○○○○○○○</a></cite>
-        </figcaption>
-      </figure>
-
-      <div class="information__image-outer">
-        <img class="information__image Image" src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="ハンバーガーとポテトの写真">
-      </div>
-    </section>
-
-    <!--ここからカード-->
-    <div class="article--single">
-      <article class="card--single Grid">
-        <div class="card--single__header">
-          <div class="card--single__figure">
-            <div class="card--single__image Image">
-            </div>
-          </div>
-        </div>
-        <div class="card--single__body">
-          <p class="card--single__content Content">
-            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-        </div>
-      </article>
-
-      <article class="card--singleSecond Grid">
-        <div class="card--singleSecond__header">
-          <div class="card--singleSecond__figure">
-            <div class="card--singleSecond__image Image">
-            </div>
-          </div>
-        </div>
-        <div class="card--singleSecond__body">
-          <p class="card--singleSecond__content Content">
-            テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-        </div>
-      </article>
-    </div>
-
-    <!--ここはimg-->
-    <div class="image__outer--single">
-      <img class="image__inner--single Image" src="<?php echo get_theme_file_uri('/image\single\cooked-foods-750073.jpg'); ?>" alt="ハンバーガーとポテトの写真">
-    </div>
-
-    <!--ここからimgリスト-->
-    <div class="image-list__wrapper">
-      <ul class="image-list Grid">
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-        <li class="image-list__item">
-          <img src="<?php esc_url(get_template_directory_uri()); ?>/image\single\cooked-foods-750073.jpg" alt="">
-        </li>
-      </ul>
-    </div>
-
-    <!--list-->
-    <div class="list-wrapper">
-      <div class="decimal-list__outer">
-        <ol class="decimal-list">
-          <li class="decimal-list__item">リストリストリスト</li>
-          <li class="decimal-list__item">リストリストリスト
-            <ol class="decimal-list decimal-list--second">
-              <li class="decimal-list__item decimal-list__item--second">リスト2リスト2リスト2</li>
-              <li class="decimal-list__item decimal-list__item--second">リスト2リスト2リスト2</li>
-            </ol>
-          </li>
-        </ol>
-        <ol class="decimal-list">
-          <li class="decimal-list__item">リストリストリスト</li>
-          <li class="decimal-list__item">リストリストリスト</li>
-        </ol>
-      </div>
-      <div class="text-list__outer">
-        <ul class="text-list">
-          <li class="text-list__item">リストリストリスト</li>
-          <li class="text-list__item">リストリストリスト
-            <ul class="text-list text-list--second">
-              <li class="text-list__item text-list__item--second">リスト2リスト2リスト2</li>
-              <li class="text-list__item text-list__item--second">リスト2リスト2リスト2</li>
-            </ul>
-          </li>
-        </ul>
-        <ul class="text-list">
-          <li class="text-list__item">リストリストリスト</li>
-          <li class="text-list__item">リストリストリスト</li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="code">
-      <pre class="code__outer">
+  <div class="code">
+    <pre class="code__outer">
 <code class="code__inner">&lt;html&gt;
     &lt;head&gt;
     &lt;/head&gt;
     &lt;body&gt;
     &lt;/body&gt;
 &lt;/html&gt;</code></pre>
-    </div>
+  </div>
 
-    <div class="table">
-      <table class="table__body Table" border="1">
-        <tr>
-          <th class="table__colum--one">テーブル</th>
-          <th class="table__colum--two">テーブル</th>
-        </tr>
-        <tr>
-          <th class="table__colum--one">テーブル</th>
-          <th class="table__colum--two">テーブル</th>
-        </tr>
-        <tr>
-          <th class="table__colum--one">テーブル</th>
-          <th class="table__colum--two">テーブル</th>
-        </tr>
-        <tr>
-          <th class="table__colum--one">テーブル</th>
-          <th class="table__colum--two">テーブル</th>
-        </tr>
-      </table>
-    </div>
+  <div class="table">
+    <table class="table__body Table" border="1">
+      <tr>
+        <th class="table__colum--one">テーブル</th>
+        <th class="table__colum--two">テーブル</th>
+      </tr>
+      <tr>
+        <th class="table__colum--one">テーブル</th>
+        <th class="table__colum--two">テーブル</th>
+      </tr>
+      <tr>
+        <th class="table__colum--one">テーブル</th>
+        <th class="table__colum--two">テーブル</th>
+      </tr>
+      <tr>
+        <th class="table__colum--one">テーブル</th>
+        <th class="table__colum--two">テーブル</th>
+      </tr>
+    </table>
+  </div>
 
-    <div class="button-foot">
-      <button class="button-foot__body Button">
-        ボタン
-      </button>
-    </div>
+  <div class="button-foot">
+    <button class="button-foot__body Button">
+      ボタン
+    </button>
+  </div>
 
-    <div class="text-foot Heading--m1bold">
-      <p>boldboldboldboldboldboldboldbold</p>
-    </div>
+  <div class="text-foot Heading--m1bold">
+    <p>boldboldboldboldboldboldboldbold</p>
+  </div>
 
-  </main>
+</main>
 
-  <!--footer-->
-  <?php get_footer(); ?>
+<!--footer-->
+<?php get_footer(); ?>
