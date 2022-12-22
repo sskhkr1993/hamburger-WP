b7c187efb3590313cd898ad159d514ce833da747
sskhkr1993 <krkkou65@gmail.com>
2022-12-22 16:25:11 +0900

    検索フォーム修正

--- searchform.php
+++ searchform.php
@@ -1,6 +1,10 @@
-<form class="seach" id="searchform" method="get" action="<?php echo esc_url(home_url('/')) ; ?>">
+<form class="seach" id="searchform" method="get" action="<?php echo esc_url(home_url('/')); ?>">
   <div class="seach__icon">
-    <input class="seach__box" type="search" name="s" id="s">
+    <input class="seach__box" type="search" name="s" id="s" placeholder="<?php if (!is_search()) {
+                                                                            echo 'バーガー';
+                                                                          } ?>" value="<?php if (is_search()) {
+                                  echo get_search_query();
+                                } ?>">
   </div>
   <input class="seach__submit" id="searchsubmit" type="submit" value="検索">
 </form>
